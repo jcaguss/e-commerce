49e3330e203dba3d6705c276a436ab2117ea55dd
Agustin Caceres <agustincaceresm@gmail.com>
2023-11-04 20:10:09 -0300

    controllers

---
+++ src/controllers/products.controller.js
@@ -0,0 +1,66 @@
+import { productModel } from "../models/products.models.js";
+
+export const getProduct = async (req,res) => {
+    const query = req.query.query;
+    const limit = parseInt(req.query.limit) || 10;
+    const page = parseInt(req.query.page) || 1;
+    const sort = req.query.sort || '';
+    try{
+        const prods = await productModel.paginate({query},{ limit, page, sort: { price: sort }})
+        !prods 
+        ? res.status(404).send({error: "Productos no encontrados"})
+        : res.status(200).send({respuesta: 'ok', mensaje: prods})
+    }catch(error){
+        res.status(500).send({error:`Error en consultar productos ${error}`})
+    }
+}
+
+export const getProductById = async (req,res) => {
+    const {id} = req.params
+    try{
+        const prods = await productModel.findById(id)
+        !prods 
+        ? res.status(404).send({respuesta: 'error', mensaje: "Producto no encontrado"}) 
+        : res.status(200).send({respuesta: 'ok', mensaje: prods})
+    }catch(error){
+        res.status(500).send({error:`Error en consultar producto ${error}`})
+    }
+}
+
+export const postProduct = async (req,res) => {
+    const {title, description, code, price, stock, category} = req.body
+    try{
+        const prod = await productModel.create({title, description, code, price, stock, category})
+        !prod
+        ? res.status(400).send({error: "Error en crear Producto"})
+        : res.status(201).send({respuesta: 'ok', mensaje: prod})
+    }catch(error){
+        error.code == 11000 // -El error code "11000" es de llave duplicada-
+        ? res.status(400).send({error: "Producto ya creado con llave duplicada"})
+        : res.status(500).send({error:`Error en crear Producto ${error}`})
+    }
+}
+export const putProductById = async (req,res) => {
+    const {id} = req.params
+    const {title, description, code, price, status, stock, category} = req.body
+    try{
+        const prod = await productModel.findByIdAndUpdate(id, {title, description, code, price, status, stock, category})
+        !prod 
+        ? res.status(404).send({error: "Producto no encontrado"}) 
+        : res.status(200).send({respuesta: 'ok', mensaje: prod})
+    }catch(error){
+        res.status(500).send({error:`Error en actualizar producto ${error}`})
+    }
+}
+
+export const deleteProductById = async (req,res) => {
+    const {id} = req.params
+    try{
+        const prod = await productModel.findByIdAndDelete(id)
+        !prod 
+        ? res.status(404).send({error: "Producto no encotrado"}) 
+        : res.status(200).send({respuesta: 'ok', mensaje: prod})
+    }catch(error){
+        res.status(500).send({error:`Error en eliminar producto ${error}`})
+    }
+}
