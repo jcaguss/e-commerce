c29f3d21acdd267c04eddb921e9a10b05345b469
Agustin Caceres <agustincaceresm@gmail.com>
2023-12-29 15:11:31 -0300

    Loggers-Artillery

--- e-commerce/backend/src/config/logger.js
+++ e-commerce/backend/src/config/logger.js
@@ -1,70 +1,100 @@
 import winston from "winston";
 
 const customLevelOpt = {
-    levels: {
-        fatal: 0,
-        error: 1,
-        warning: 2,
-        info: 3,
-        debug: 4
-    },
-    colors: {
-        fatal: "red",
-        error: "yellow",
-        warning: "cyan",
-        info: "blue",
-        debug: "gray"
-    }
-}
-
+  levels:
+    process.env.MODE === "development"
+      ? {
+          fatal: 0,
+          error: 1,
+          warning: 2,
+          info: 3,
+          debug: 4,
+        }
+      : {
+          fatal: 0,
+          error: 1,
+          warning: 2,
+          info: 3,
+        },
+  colors:
+    process.env.MODE === "development"
+      ? {
+          fatal: "red",
+          error: "yellow",
+          warning: "cyan",
+          info: "blue",
+          debug: "grey",
+        }
+      : {
+          fatal: "red",
+          error: "yellow",
+          warning: "cyan",
+          info: "blue",
+        }
+};
 const logger = winston.createLogger({
-    levels: customLevelOpt.levels,
-    transports: [
-        new winston.transports.File({
-            filename: './errors.log',
-            level: 'fatal',
-            format: winston.format.combine(
-                winston.format.colorize({colors: customLevelOpt.colors}),
-                winston.format.simple()
-            )
-        }),
-        new winston.transports.File({
-            filename: './loggers.log',
-            level: 'warning',
-            format: winston.format.combine(
-                winston.format.colorize({colors: customLevelOpt.colors}),
-                winston.format.simple()
-            )
-        }),
-        new winston.transports.File({
-            filename: './loggers.log',
-            level: 'info',
-            format: winston.format.combine(
-                winston.format.colorize({colors: customLevelOpt.colors}),
-                winston.format.simple()
-            )
-        }),
-        new winston.transports.File({
-            filename: './loggers.log',
-            level: 'warning',
-            format: winston.format.combine(
-                winston.format.colorize({colors: customLevelOpt.colors}),
-                winston.format.simple()
-            )
-        }),
-        new winston.transports.Console({
-            level: 'debug',
-            format: winston.format.combine(
-                winston.format.colorize({colors: customLevelOpt.colors}),
-                winston.format.simple()
-            )
-        })
-    ]
-    
-})
+  levels: customLevelOpt.levels,
+  transports: 
+  process.env.MODE === "development"
+  ? [
+    new winston.transports.File({
+      filename: "./errors.html",
+      level: "fatal",
+      format: winston.format.combine(winston.format.simple()),
+    }),
+    new winston.transports.File({
+      filename: "./errors.html",
+      level: "error",
+      format: winston.format.combine(winston.format.simple()),
+    }),
+    new winston.transports.File({
+      filename: "./loggers.html",
+      level: "warning",
+      format: winston.format.combine(winston.format.simple()),
+    }),
+    new winston.transports.File({
+      filename: "./loggers.html",
+      level: "info",
+      format: winston.format.combine(winston.format.simple()),
+    }),
+    new winston.transports.Console({
+      level: "debug",
+      format: winston.format.combine(
+        winston.format.colorize({ colors: customLevelOpt.colors }),
+        winston.format.simple()
+      )
+    })
+  ]
+  : [
+    new winston.transports.File({
+      filename: "./errors.html",
+      level: "fatal",
+      format: winston.format.combine(winston.format.simple()),
+    }),
+    new winston.transports.File({
+      filename: "./errors.html",
+      level: "error",
+      format: winston.format.combine(winston.format.simple()),
+    }),
+    new winston.transports.File({
+      filename: "./loggers.html",
+      level: "warning",
+      format: winston.format.combine(winston.format.simple()),
+    }),
+    new winston.transports.File({
+      filename: "./loggers.html",
+      level: "info",
+      format: winston.format.combine(winston.format.simple()),
+    })
+  ]
+});
 
-export const addLogger = (req,res,next) => {
-    req.logger = logger
-    req.logger.info(`${req.method} es ${req.url} - ${new Date().toLocaleTimeString()}`)
-    next()
-}
+export const addLogger = (req, res, next) => {
+  req.logger = logger;
+  if(process.env.MODE === "development"){
+    req.logger.debug(
+        `${req.method} es ${req.url} - ${new Date().toLocaleTimeString()}`
+      );
+  }
+  next();
+};
--- e-commerce/backend/src/index.js
+++ e-commerce/backend/src/index.js
@@ -1,166 +1,175 @@
-import 'dotenv/config'
+import "dotenv/config";
 import express from "express";
-import nodemailer from 'nodemailer'
-import cors from 'cors';
-import compression from 'express-compression';
+import nodemailer from "nodemailer";
+import cors from "cors";
+import compression from "express-compression";
 import mongoose from "mongoose";
-import { addLogger } from './config/logger.js';
-import swaggerJSDoc from 'swagger-jsdoc';
-import swaggerUiExpress from 'swagger-ui-express';
-import cookieParser from 'cookie-parser';
-import passport from 'passport';
-import initializePassport from './config/passport.js'
-import session from 'express-session';
-import router from './routes/index.routes.js';
-import MongoStore from 'connect-mongo';
+import { addLogger } from "./config/logger.js";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUiExpress from "swagger-ui-express";
+import cookieParser from "cookie-parser";
+import passport from "passport";
+import initializePassport from "./config/passport.js";
+import session from "express-session";
+import router from "./routes/index.routes.js";
+import MongoStore from "connect-mongo";
 // import { engine } from "express-handlebars";
 // import { Server } from "socket.io";
 import { __dirname } from "./path.js";
-import  path  from "path";
+import path from "path";
 
-const whiteList = ['http://localhost:5173']
+
+const whiteList = ["http://localhost:5173"];
 
 const corsOptions = {
-    origin: function(origin,callback){
-        if(whiteList.indexOf(origin) != -1 || !origin){
-            callback(null,true)
-        }else{
-            callback(new Error("Acceso denegado"))
-        }
+  origin: function (origin, callback) {
+    if (whiteList.indexOf(origin) != -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Acceso denegado"));
     }
-}
-
-
+  },
+};
 
 // ---- Server ----
 const app = express();
 const PORT = 8080;
 
 const server = app.listen(PORT, () => {
-    console.log(`Server on port ${PORT}`)
-})
+  console.log(`Server on port ${PORT}`);
+});
 
 let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-        user: 'jcaceresm1999@gmail.com',
-        pass: process.env.PASSWORD_EMAIL,
-        authMethod: 'LOGIN'
-    }
-})
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "jcaceresm1999@gmail.com",
+    pass: process.env.PASSWORD_EMAIL,
+    authMethod: "LOGIN",
+  },
+});
 
-app.get('/mail', async(req,res) =>{
-    const respuesta = await transporter.sendMail({
-        from: 'TEST MAIL jcaceresm1999@gmail.com',
-        to: 'jcaceresm1999@gmail.com',
-        subject: 'Hola, buenas tardes',
-        html: 
-        `
+app.get("/mail", async (req, res) => {
+  const respuesta = await transporter.sendMail({
+    from: "TEST MAIL jcaceresm1999@gmail.com",
+    to: "jcaceresm1999@gmail.com",
+    subject: "Hola, buenas tardes",
+    html: `
             <div>
                 <h1>Mi Primer Mail</h1>
             </div>
         `,
-        attachments:[{
-            filename: 'msRobot.jpg',
-            path: __dirname + '/img/msRobot.jpg',
-            cid: 'msRobot.jpg'
-        }]
-    })
-    console.log(respuesta)
-    res.send('Email enviado')
-})
+    attachments: [
+      {
+        filename: "msRobot.jpg",
+        path: __dirname + "/img/msRobot.jpg",
+        cid: "msRobot.jpg",
+      },
+    ],
+  });
+  console.log(respuesta);
+  res.send("Email enviado");
+});
 
 //---------
 
 const swaggerOptions = {
-    definition: {
-        openapi:'3.1.0',
-        info: {
-            title: 'Documentacion del curso de Backend',
-            description: 'Api Coder Backend'
-        }
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "Documentacion del curso de Backend",
+      description: "Api Coder Backend",
     },
-    apis: [`${__dirname}/docs/**/*.yaml`] //** indica una subcarpeta que no importa el nombre, *. no interesa el nombre si la extencion 
-}
+  },
+  apis: [`${__dirname}/docs/**/*.yaml`], //** indica una subcarpeta que no importa el nombre, *. no interesa el nombre si la extencion
+};
 
-const specs = swaggerJSDoc(swaggerOptions)
-app.use('/apidocs', swaggerUiExpress.serve, swaggerUiExpress.setup(specs))
-
+const specs = swaggerJSDoc(swaggerOptions);
+app.use("/apidocs", swaggerUiExpress.serve, swaggerUiExpress.setup(specs));
 
 // ---- BBD ----
-mongoose.connect(process.env.MONGO_URL)
-.then(async() => {
-    console.log('BDD conectada')
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(async () => {
+    console.log("BDD conectada");
     // const res = await cartModel.findOne({_id:'64ff5f57ddf4e37b81ab3de0'})
     // console.log(JSON.stringify(res))
-})
-.catch(()=>console.log("error en conectar en BDD"))
-
+  })
+  .catch(() => console.log("error en conectar en BDD"));
 
 // ---- Middlewares ----
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 //----------------
-app.use(express.json())
-app.use(express.urlencoded({ extended:true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 //----------------
-app.set('views', path.resolve(__dirname, './views'))//Path
+app.set("views", path.resolve(__dirname, "./views")); //Path
 //----------------
 // app.use('/login', express.static(path.join(__dirname, "/public")))
 //-------compression---------
-app.use(compression({
-    brotli: {endeble:true, zlib: {}}
-}))
+app.use(
+  compression({
+    brotli: { endeble: true, zlib: {} },
+  })
+);
 //----------------
-app.use(cookieParser(process.env.SIGNED_COOKIE)) // Firma Cookie
+app.use(cookieParser(process.env.SIGNED_COOKIE)); // Firma Cookie
 //------sessions-------
-app.use(session({
+app.use(
+  session({
     store: MongoStore.create({
-        mongoUrl: process.env.MONGO_URL,
-        mongoOptions: {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        },
-        ttl: 60
+      mongoUrl: process.env.MONGO_URL,
+      mongoOptions: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+      ttl: 60,
     }),
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false
-}))
+    saveUninitialized: false,
+  })
+);
 //----- Passport -----
-initializePassport()
-app.use(passport.initialize())
-app.use(passport.session())
+initializePassport();
+app.use(passport.initialize());
+app.use(passport.session());
 
 //----- Rutas -----
-app.use('/', router)
+app.use("/", router);
 
 //----- Loggers -----
-app.use(addLogger)
+app.use(addLogger);
 
 //---------------------------
-app.get('/',(req,res) => {
-    req.logger.fatal('fatal')
-    res.send('fatal')
-})
-app.get('/',(req,res) => {
-    req.logger.error('error')
-    res.send('error')
-})
-app.get('/',(req,res) => {
-    req.logger.warning('warning')
-    res.send('warning')
-})
-app.get('/',(req,res) => {
-    req.logger.info('info')
-    res.send('info')
-})
-app.get('/',(req,res) => {
-    req.logger.debug('debug')
-    res.send('debug')
-})
+app.get("/info", (req, res) => {
+  req.logger.info(
+    '<span style="color:blue">Texto informativo de Info</span><br/>'
+  );
+  res.send("info");
+});
 
+app.get("/warning", (req, res) => {
+  req.logger.warning('<span style="color:cyan">Texto Warning</span><br/>');
+  res.send("warning");
+});
+
+app.get("/error", (req, res) => {
+  req.logger.error('<span style="color:yellow">Texto Error</span><br/>');
+  res.send("error");
+});
+
+app.get("/fatal", (req, res) => {
+  req.logger.fatal(
+    '<span style="color:red">Texto Fatal</span><br/>'
+  );
+  res.send("fatal");
+});
+
+app.get("/testArtillery", (req, res) => {
+  res.send("Hola desde Artillery");
+});
 
 // ---- Server socket.io ----
 
